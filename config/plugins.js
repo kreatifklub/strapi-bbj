afe2d7f747869d7dffff303f6920f4cd896bd9bb
Artyom Yakovlev <maniakmill@gmail.com>
2025-10-15 18:49:40 +0300

    Add bunny cdn

--- config/plugins.js
+++ config/plugins.js
@@ -9,7 +9,21 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: "local",
+      provider: '@nexide/strapi-provider-bunny',
+      providerOptions: {
+        api_key: env('BUNNY_API_KEY'),
+        storage_zone: env('BUNNY_STORAGE_ZONE'),
+        pull_zone: env('BUNNY_PULL_ZONE'),
+        hostname: env('BUNNY_HOSTNAME'),
+        upload_path: env('BUNNY_UPLOAD_PATH'),
+      },
+      breakpoints: {
+        xlarge: 2560,
+        large: 1920,
+        medium: 1200,
+        small: 768,
+        xsmall: 64,
+      },
       actionOptions: {
         upload: {},
         delete: {},
